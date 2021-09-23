82ca851455574ddc5f6fd96739e73c46af6c5e11
DIEP PHAM THI <1821050841@student.humg.edu.vn>
2021-09-23 10:53:16 +0700

    Code

---
+++ Data/MvcMovieContext.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DemoMVC.Models;
+namespace DemoMVC.Data{
+     public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+ 
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
